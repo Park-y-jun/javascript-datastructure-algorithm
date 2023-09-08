b3d2f4ddcbd20bc7b6d3510270c5bc7a89b05d41
smith <aptl741@gmail.com>
2023-09-08 20:54:05 +0900

    이중연결 리스트 shift()

--- DataStructure/doublyLinkedList.js
+++ DataStructure/doublyLinkedList.js
@@ -74,4 +74,21 @@
       this.length--;
       return shiftNode;
     }
+
+    unshift(value) {
+      //새로운 노드만들어서 연결만 해주면 되는거 아닌감
+      const newNode = new Node(value);
+
+      if (this.length === 0) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+
+      this.length++
+      return this;
+    }
   }
